c85865e103a607d6c400738f1af41ec43d97b14c
IamLAM <ingeniero@luismonge.com.mx>
2019-06-02 23:07:34 -0700

    letters detected

--- code.js
+++ code.js
@@ -7,8 +7,32 @@ $("body").on("keypress",function(event){
     var letter=event.keyCode;
     console.log(letter);
     switch(letter){
-        case 113://Q
+        case 81://Q
         $("#Q").trigger("play");
+        break;
+        case 87://W
+        $("#W").trigger("play");
+        break;
+        case 69://E
+        $("#E").trigger("play");
+        break;
+        case 65://A
+        $("#A").trigger("play");
+        break;
+        case 83://S
+        $("#S").trigger("play");
+        break;
+        case 68://D
+        $("#D").trigger("play");
+        break;
+        case 90://Z
+        $("#Z").trigger("play");
+        break;
+        case 88://X
+        $("#X").trigger("play");
+        break;
+        case 67://C
+        $("#C").trigger("play");
         break;
         default:
             console.log("no founded"); 

4e3b476d07deb2b96f4941d65630a08c78cc3217
IamLAM <ingeniero@luismonge.com.mx>
2019-06-02 23:11:18 -0700

    uppercase and lowercase added

--- code.js
+++ code.js
@@ -7,31 +7,31 @@ $("body").on("keypress",function(event){
     var letter=event.keyCode;
     console.log(letter);
     switch(letter){
-        case 81://Q
+        case 81: case 113://Q 
         $("#Q").trigger("play");
         break;
-        case 87://W
+        case 87: case 119://W
         $("#W").trigger("play");
         break;
-        case 69://E
+        case 69: case 101://E
         $("#E").trigger("play");
         break;
-        case 65://A
+        case 65: case 97://A
         $("#A").trigger("play");
         break;
-        case 83://S
+        case 83: case 115://S
         $("#S").trigger("play");
         break;
-        case 68://D
+        case 68: case 100://D
         $("#D").trigger("play");
         break;
-        case 90://Z
+        case 90: case 122://Z
         $("#Z").trigger("play");
         break;
-        case 88://X
+        case 88: case 120://X
         $("#X").trigger("play");
         break;
-        case 67://C
+        case 67: case 99://C
         $("#C").trigger("play");
         break;
         default:
